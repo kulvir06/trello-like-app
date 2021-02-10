f1e76087bdf99eed33ddc0084962ab03a22f4753
Kulvir Singh <kulvir.singh2019@vitstudent.ac.in>
2021-02-10 14:55:48 +0530

    delete board and corresponding lists

--- routes/board.js
+++ routes/board.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import randomNumberGenerator from '../utils/randomGen';
 import emailExtractor from '../utils/emailExtractor';
 import createBoard from '../services/createBoard';
+import deleteBoard from '../services/deleteBoard';
+import listExtractor from '../utils/listExtractor';
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({extended: true}));
@@ -17,6 +19,16 @@ router.post('/', (req,res) => {
     let members = emailExtractor(req.body.emails);
     let id = randomNumberGenerator();
     members.forEach(member => obj.addBoard(name,member,id));    
+});
+
+router.get('/delete/:id', async (req,res) => {
+    let name = req.params.id;
+    const obj = new deleteBoard();
+    let listIds = await listExtractor(name);
+    listIds = listIds.split(',');
+    await obj.deleteList(listIds);
+    await obj.deleteBoard(name);
+
 });
 
 
---
+++ services/deleteBoard.js
@@ -0,0 +1,32 @@
+import db from '../utils/database';
+
+class remove{
+    deleteBoard(name){
+        return new Promise(async resolve => {
+            try{
+                const del = await db.board.destroy({ where: { name } })
+                 .then(() => console.log('board deleted'))
+                 .catch((err) => console.log('error occured = '+err));
+                resolve(del);
+            } catch(err) {
+                console.log('error occured = '+err);
+            }
+        })
+    }
+    deleteList(listIds){
+        return new Promise(async resolve => {
+            try{
+                listIds.forEach(async listId => {
+                    const del = await db.list.destroy({ where: {id: listId }})  
+                     .then(() => console.log('list deleted'))
+                     .catch((err) => console.log('error occured = '+err));
+                    resolve(del);                                  
+                });
+            } catch(err) {
+                console.log('error occured = '+err);
+            }
+        });
+    }
+}
+
+module.exports = remove;
